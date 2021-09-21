3e9049dadf9a32e17bbe8bd5f2f2105f4319329c
darakus1420 <darakus1420@gmail.com>
2021-09-21 12:47:25 -0300

    fix: fix types and return structure

--- python_app/modules/ImageRecognition/ImageRecognition.py
+++ python_app/modules/ImageRecognition/ImageRecognition.py
@@ -5,15 +5,18 @@ import numpy as np
 class ImageRecognition:
 
     @staticmethod
-    def converTohsv(img):
+    def conver_to_hsv(img):
         hsvScale = cv.cvtColor(img, cv.COLOR_BGR2HSV)
         return hsvScale
 
     @staticmethod
-    def centroidDetected(img, maskin):
+    def centroid_detected(img, maskin) -> list:
+
+        particles = []
         mask = cv.medianBlur(maskin, 7)
         contours = cv.findContours(
             mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
+
         for i in range(len(contours)):
             cnt = contours[i]
             area = cv.contourArea(cnt)
@@ -22,10 +25,12 @@ class ImageRecognition:
                 cX = int(M["m10"]/M["m00"])
                 cY = int(M["m01"]/M["m00"])
                 cv.circle(img, (cX, cY), 3, (0, 255, 0), -1)
+                particles.append({'centroid': (cX, cY)})
+        return particles
 
     @staticmethod
-    def redParticles(img):
-        hsvScale = ImageRecognition.converTohsv(img)
+    def found_red_particles(img) -> list:
+        hsvScale = ImageRecognition.conver_to_hsv(img)
 
         lowRed1 = np.array([0, 100, 20], np.uint8)
         highRed1 = np.array([8, 255, 255], np.uint8)
@@ -36,30 +41,42 @@ class ImageRecognition:
         redMask2 = cv.inRange(hsvScale, lowRed2, highRed2)
         redMask = cv.add(redMask1, redMask2)
 
-        ImageRecognition.centroidDetected(img, redMask)
-
-        return redMask
+        return ImageRecognition.centroid_detected(img, redMask)
 
     @staticmethod
-    def blueParticles(img):
-        hsvScale = ImageRecognition.converTohsv(img)
+    def found_blue_particles(img) -> list:
+        hsvScale = ImageRecognition.conver_to_hsv(img)
 
         lowBlue = np.array([100, 100, 25], np.uint8)
         highBlue = np.array([125, 255, 255], np.uint8)
 
         blueMask = cv.inRange(hsvScale, lowBlue, highBlue)
 
-        ImageRecognition.centroidDetected(img, blueMask)
+        return ImageRecognition.centroid_detected(img, blueMask)
 
-        return blueMask
+    @staticmethod
+    def found_all_particles(img) -> dict:
+        blue_particles = ImageRecognition.found_blue_particles(img)
+        red_particles = ImageRecognition.found_red_particles(img)
+        response = {
+            'blue_particles':
+                {
+                    'particles': blue_particles, 'length': len(blue_particles)
+                },
+            'red_particles':
+                {
+                    'particles': red_particles, 'length': len(red_particles)
+                }
+
+        }
+        return response
 
 
-imagen = cv.imread(
-    '/home/eliacer/Descargas/Phantom-Cyto-Seq/Seq/Img000038.jpg')
+if __name__ == '__main__':
+    imagen = cv.imread(
+        'Img000038.jpg')
 
-
-ImageRecognition.blueParticles(imagen)
-ImageRecognition.redParticles(imagen)
-cv.imshow("imagen", imagen)
-cv.waitKey(0)
-cv.destroyAllWindows()
+    ImageRecognition.found_all_particles(imagen)
+    cv.imshow("imagen", imagen)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
